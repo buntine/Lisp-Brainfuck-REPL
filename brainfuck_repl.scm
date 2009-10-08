48749cc56111f7725b0b6551707b8ee5f50e763d
buntine <andrew.buntine@ansearch.com>
2009-10-08 15:55:11 +1100

    Initial commit. A working echo REPL...

---
+++ brainfuck_repl.scm
@@ -0,0 +1,31 @@
+; Main REPL looping procedure.
+(define mainloop
+  (lambda ()
+    (begin 
+      (display "> ")
+      (display (read-input))
+      (newline))
+  (mainloop)))
+
+; Reads an arbitrary string of input and returns a list of chars.
+(define read-input
+  (lambda ()
+    (let ((in (read-line)))
+      (string->list in))))
+
+; Reads a line of chars from STDIN port. Returns a string.
+; Credit: Nils M Holm (http://www.bcl.hamilton.ie/~nmh/t3x.org/zzz).
+(define (read-line)
+  (letrec
+    ((collect-chars
+       (lambda (c s)
+         (cond ((eof-object? c)
+                 (if (null? s)
+                     c
+                     (apply string (reverse s))))
+               ((char=? c #\newline)
+                 (apply string (reverse s)))
+               (else
+                 (collect-chars (read-char)
+                                (cons c s)))))))
+    (collect-chars (read-char) '())))
